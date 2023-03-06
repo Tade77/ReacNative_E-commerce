b3bc07bf88b9d3510ba5cd662d51e9f950afe8a0
Tade77 <adekunletaofeeq@mail.com>
2023-03-06 19:45:42 +0100

    WIP: Login Screen

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Image,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imgBg}>
+        <Image
+          style={styles.img}
+          source={require("./assets/shopping-bags.png")}
+        />
+        <Text style={styles.logoText}>Tade's Shop</Text>
+      </View>
+      <TextInput style={styles.inputs} placeholder="Phone Number" />
+      <TextInput style={styles.inputs} placeholder="Password" />
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +29,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#78FF86",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imgBg: {
+    marginBottom: 122,
+    // height: 80,
+    // width: 80,
+  },
+  img: {
+    marginLeft: 25,
+  },
+  logoText: {
+    fontSize: 24,
+    lineHeight: 24,
+    color: "#fff",
+  },
+  inputs: {
+    width: 320,
+    height: 45,
+    borderRadius: 10,
+    marginBottom: 21,
+    backgroundColor: "white",
   },
 });
